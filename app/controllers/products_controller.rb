2d8427b0e576e6fbc2e85cf4a47513b5d1b69220
TakeumiYamamura <yama1223xxx@sansontakeshi-no-MacBook-Air.local>
2015-04-04 14:39:48 +0900

    料理投稿フォーム作成

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,22 @@ class ProductsController < ApplicationController
   def index
     @products = Product.page(params[:page]).per(8)
   end
+
   def new
   end
+
+  def create
+    Product.create(title: create_params[:title], explanation: create_params[:explanation], price: create_params[:price], capacity: create_params[:capacity],avatar: create_params[:avatar], date: create_params[:date])
+    redirect_to "/products"
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  private
+  def create_params
+    params.permit(:title, :explanation, :price, :capacity, :avatar, :date)
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150326053235) do
+ActiveRecord::Schema.define(version: 20150404034243) do
 
   create_table "images", force: true do |t|
     t.datetime "created_at"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20150326053235) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.date     "date"
   end
 
   create_table "users", force: true do |t|
