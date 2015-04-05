58e71eb39078c245d98c35745f56c0f53b0da8f3
TakeumiYamamura <yama1223xxx@sansontakeshi-no-MacBook-Air.local>
2015-04-05 15:01:12 +0900

    間違ったmigrationファイル削除

--- db/migrate/20150404102631_rename_user_id_column_to_books.rb
+++
@@ -1,8 +0,0 @@
-class RenameUserIdColumnToBooks < ActiveRecord::Migration
-  def up
-    rename_column :books, :user_id, :books_user_id
-  end
-  def down
-    rename_column :books, :books_user_id, :user_id
-  end
-end
