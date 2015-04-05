584943e35558d49a10565f4ed5a320a84a59fc74
TakeumiYamamura <yama1223xxx@sansontakeshi-no-MacBook-Air.local>
2015-04-05 15:24:11 +0900

    ログアウトパス変更　icon導入　

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150404102631) do
+ActiveRecord::Schema.define(version: 20150405034842) do
 
   create_table "books", force: true do |t|
     t.integer  "user_id"
