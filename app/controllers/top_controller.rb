73d113132fb73f5e7e9f6ca7b62ee44a7441c060
TakeumiYamamura <yama1223xxx@sansontakeshi-no-MacBook-Air.local>
2015-04-11 23:56:26 +0900

    検索対策 topビュー

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,6 +1,7 @@
 class TopController < ApplicationController
   def index
     @products = Product.all.order("id DESC").limit(5)
+    @cooks = User.where(user_type: [1,2]).limit(5) #enumで楽にできないかな？
   end
 
   def show
