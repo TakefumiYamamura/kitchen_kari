444f16169205dcd851da44af8434d07b643d437d
TakeumiYamamura <yama1223xxx@sansontakeshi-no-MacBook-Air.local>
2015-05-30 14:33:45 +0900

    定数追加

--- config/initializers/setting.rb
+++ config/initializers/setting.rb
@@ -1,2 +1,4 @@
 WEBPAY_PUBLIC_KEY = 'test_public_cUSdOXe6R0B61AJ7R059XgxH'
 WEBPAY_SECRET_KEY = 'test_secret_d079St5o73UQ3LK2U2baScDh'
+WDAY = ["日", "月", "火", "水", "木", "金", "土"]
+WCLASS = ["sun", "dayCell", "dayCell", "dayCell", "dayCell", "dayCell", "sat"]
